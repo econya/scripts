cc11a794b7e039c6c5798c238a7312708a7af251
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-03-01 16:28:54 +0100

    add redmine-builder script and README entry for it.

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ provision an Ubuntu 16.04 machine using virt-builder.
 
 Example: `ubuntu-16.04-builder.sh app-test`
 Results in the file `app-test.qcow2` which is a bare metal Ubuntu 16.04 installation.
+
+### redmine-builder.sh
+
+provision an Ubuntu 16.04 machine using virt-builder that comes slightly prepared for a redmine installation.
+
+Example: `redmine-builder.sh projects-test`
+Results in the file `projects-test.qcow2` which is a Ubuntu 16.04 installation with some additional packages installed.
---
+++ scripts/virt-helpers/redminevm-builder.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Create an opinionated Ubuntu 16.04 VM disk to host redmine, using virt-builder.
+# Copyright 2017, Felix Wolfsteller
+# License: GPLv3+
+
+# Exit on errors
+set -e
+
+if [ $# -ne 1 ]
+then
+  echo "Need to specify an argument (name)!"
+  exit 1
+fi
+
+GUESTNAME="$1"
+IMAGENAME="$1".qcow2
+
+# Simple file check (race condition prone)
+if [ -e "$IMAGENAME" ]
+then
+  echo "File $IMAGENAME already exists, exiting!"
+  exit 1
+fi
+
+# /etc/default/grub manipulation and run-command definition from:
+# `virt-builder --notes ubuntu-16.04`
+virt-builder \
+  ubuntu-16.04 \
+  --output "$IMAGENAME" \
+  --format qcow2 \
+  --hostname "$GUESTNAME" \
+  --edit '/etc/default/grub:
+          s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="console=tty0 console=ttyS0,115200n8"/' \
+  --edit '/etc/network/interfaces:
+          s/ens2/ens3/' \
+  --run-command "update-grub" \
+  --run-command "useradd -m -p '' redmine ; chage -d redmine " \
+  --firstboot-install "mysql-server" \
+  --install "mysql-client,libmysqlclient-dev,git-core,subversion,imagemagick,libmagickwand-dev,libcurl4-openssl-dev" \
+  --update \
+  --size 20G
+
+# chown -R redmine /var/www
+echo "$IMAGENAME created. You can now make friends with virsh, like this: "
+echo "sudo virt-install --import --name $GUESTNAME --ram 1024 --disk path=$IMAGENAME,format=qcow2 --os-variant ubuntu16.04 --graphics none --noautoconsole"
+
+# Explicitly exit gracefully
+exit 0
